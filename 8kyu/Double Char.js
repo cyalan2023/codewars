3a01dbb7db079c3828bff9837484714a9f5b1535
Alex <cyalan2019@gmail.com>
2024-01-03 13:40:29 +0500

    8kyu

---
+++ 8kyu/Double Char.js
@@ -0,0 +1,15 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+function doubleChar(str) {
+    // Your code here
+    let double = ''
+    for (let i = 0; i < str.length;i++){
+        double += str[i] + str[i]
+    }
+    return double
+}

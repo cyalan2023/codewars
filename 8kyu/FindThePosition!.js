ce296295224a20ec6fd36808029ad8c47ed43515
Alex <cyalan2019@gmail.com>
2024-02-29 10:01:09 +0500

    8kyu

---
+++ 8kyu/FindThePosition!.js
@@ -0,0 +1,10 @@
+// When provided with a letter, return its position in the alphabet.
+//
+// Input :: "a"
+//
+// Ouput :: "Position of alphabet: 1"
+//
+function position(letter){
+//Write your own Code!
+    return `Position of alphabet: ${letter.charCodeAt(0) - 96}`
+}
